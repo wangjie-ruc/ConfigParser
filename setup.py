5a71b7527b578b9cb85cf27fec994cf8c7775064
jie.wang <jie.wang@vistel.cn>
2020-06-05 08:55:40 +0800

    requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ install_requires=['astunparse']
 
 setup(
     name='apcs',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(exclude=('test',)),
     author='jie.wang',
     author_email='jie.wang@ruc.edu.cn',
